2113bf2c361737472bb2e392831ae095f1429edd
Shahid Ameen <iamshahidameen@gmail.com>
2022-07-30 03:24:04 +0500

    Get Dynamic text from data.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [limit, setLimit] = useState(1);
+  const [paragraphs, setParagraphs] = useState([]);
+  console.log(data);
+  function generateText(e) {
+    e.preventDefault();
+    console.log(data);
+    for (let p = 1; p <= limit; p++) {
+      setParagraphs([...paragraphs, ...data]);
+    }
+  }
+
   return (
     <>
       <section className="section-center">
         <h3>tired of boring lorem ipsum?</h3>
         <form className="lorem-form">
           <label htmlFor="amount">paragraphs:</label>
-          <input type="number" name="amount" id="amount" />
-          <button className="btn">generate</button>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={limit}
+            onChange={(e) => setLimit(e.target.value)}
+          />
+          <button className="btn" onClick={(e) => generateText(e)}>
+            generate
+          </button>
         </form>
-        <article className="lorem-text">
-          <p>
-            Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
-            Carrot cake caramels pie sweet apple pie tiramisu carrot cake.
-            Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon
-            drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton
-            candy gummi bears chocolate bar cake cookie. Cupcake muffin danish
-            muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé
-            chocolate cake pastry brownie. Oat cake halvah sweet roll cotton
-            candy croissant lollipop. Macaroon tiramisu chocolate bar candy
-            candy carrot cake jelly sweet. Gummies croissant macaroon dessert.
-            Chocolate cake dragée pie.
-          </p>
-        </article>
+        {paragraphs.map((paragraph) => {
+          return (
+            <article className="lorem-text">
+              <p>{paragraph}</p>
+            </article>
+          );
+        })}
       </section>
     </>
   );
